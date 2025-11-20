2bfbb0ca2422b354fe28489914fcca407266c5e0
Juan Diego Padilla <172089930+jdpadillavigo@users.noreply.github.com>
2025-11-20 14:43:56 -0500

    Implement responsive design

--- composeApp/src/commonMain/kotlin/com/jdpadillavigo/cmp_theming/App.kt
+++ composeApp/src/commonMain/kotlin/com/jdpadillavigo/cmp_theming/App.kt
@@ -1,62 +1,14 @@
 package com.jdpadillavigo.cmp_theming
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.TextStyle
-import cmp_theming.composeapp.generated.resources.Res
-import cmp_theming.composeapp.generated.resources.compose_multiplatform
+import com.jdpadillavigo.cmp_theming.login.LoginScreen
 import com.jdpadillavigo.cmp_theming.theme.NoteMarkTheme
-import com.jdpadillavigo.cmp_theming.theme.extraColor
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     NoteMarkTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .safeContentPadding()
-                .fillMaxSize()
-                .clip(MaterialTheme.shapes.medium)
-                .background(
-                    MaterialTheme.colorScheme.extraColor
-                ),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text(
-                    text = "Click me!",
-                    style = TextStyle()
-                )
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        LoginScreen()
     }
 }
